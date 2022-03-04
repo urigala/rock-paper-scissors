8b3f23f5971f9d9cfc04c4a48f36bc4a4f220af5
uriel g <urigalarza@hotmail.com>
2022-03-03 19:56:25 -0500

    Complete logic to play against computer

--- main.js
+++ main.js
@@ -4,7 +4,65 @@ const computerPlay = () => {
 
     console.log(options[randNum])
     return options[randNum]
-
 }
 
-computerPlay()
+const playerSelection = () => {
+    const playerChoice = parseInt(prompt('Enter the number of your choice:\n 1-Rock\n 2-paper\n 3-Scissors'))
+    const options = ['Rock', 'Paper', 'Scissors']
+    console.log(options[playerChoice - 1])
+    return options[playerChoice - 1]
+}
+
+const playRound = (player, computer) => {
+
+    if ( player === computer ) {
+        console.log('Its a tie.')
+        return 0
+    } else {
+        if (player === 'Rock') {
+            if (computer === 'Paper') {
+                console.log('You lose! Paper beats rock.')
+                return 1
+            } else if (computer === 'Scissors') {
+                console.log('You win! Rock beats scissors.')
+                return 2
+            }
+        } else if (player === 'Paper') {
+            if (computer === 'Rock') {
+                console.log('You win! Paper beats rock.')
+                return 2
+            } else if (computer === 'Scissors') {
+                console.log('You lose! Scissors beat paper.')
+                return 1
+            }
+        } else if (player === 'Scissors') {
+            if (computer === 'Paper') {
+                console.log('You win! Scissors beat paper.')
+                return 2
+            } else if (computer === 'Rock') {
+                console.log('You lose! Rock beats scissors.')
+                return 1
+            }
+        }
+    }
+}
+
+const game = () => {
+    let userWins = 0
+    let compWins = 0
+
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(playerSelection(), computerPlay())
+        if (result === 0) {
+            continue
+        } else if (result === 1) {
+            compWins++
+        } else {
+            userWins++
+        }
+    }
+    console.log('Final Score: \n' + 'Player: ' + userWins + '\nComputer: ' + compWins)
+}
+
+game()
+
